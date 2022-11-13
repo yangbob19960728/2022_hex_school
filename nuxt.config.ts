28955648d672c39ab621e71441b57a9ddca713e8
BobYang <f3612453@gmail.com>
2022-11-13 22:02:20 +0800

    fix: 修正使用github-pages會出現的問題
    因為編譯的檔案或資料夾的最前面不能有"_"，而目前編譯完會有兩個檔案會有"_":
    1. _payload.js => experimental.payloadExtraction 設為false可以解決
    issue: https://github.com/nuxt/framework/issues/7769#issuecomment-1255599657
    
    2._plugin-vue_export-helper js => 藉由sanitizeFileName函數去把不合法的字元去除
    issue: https://github.com/vitejs/vite/issues/9119

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-// const runtimeConfig = useRuntimeConfig()
-const baseURL = process.env.NUXT_APP_DEPLOY_ENV === 'GH_PAGES' ? "/2022_hex_school/": "/"
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+const baseURL = process.env.NUXT_APP_DEPLOY_ENV === 'GH_PAGES' ? "/2022_hex_school/" : "/"
 export default defineNuxtConfig({
+    //https://github.com/nuxt/framework/issues/7769#issuecomment-1255599657
+    experimental: {
+        payloadExtraction: false
+    },
     app: {
         // pageTransition: { name: 'page', mode: 'out-in' },
         // layoutTransition: { name: 'layout', mode: 'out-in' }
@@ -32,6 +37,22 @@ export default defineNuxtConfig({
                     additionalData: '@use "@/assets/scss/_colors.scss" as *;'
                 }
             }
-        }
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+                    sanitizeFileName(name) {
+                        const match = DRIVE_LETTER_REGEX.exec(name);
+                        const driveLetter = match ? match[0] : '';
+                        // substr 是被淘汰語法，因此要改 slice
+                        return (
+                            driveLetter +
+                            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+                        );
+                    },
+                },
+            },
+        },
     }
 })
